206020d359d65168669880d3a212f4f73048a3e3
BrankaG <brankisa22@gmail.com>
2018-01-07 17:38:27 +0100

    Add Post migration, model, controller and sluggable feature

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Post;
 
 class IndexController extends Controller
 {
@@ -13,7 +14,9 @@ class IndexController extends Controller
      */
     public function index()
     {
-        return view('index');
+		$posts = Post::orderBy('created_at', 'DESC')->paginate(20);
+		
+        return view('index')->with('posts',$posts);
     }
 
    
@@ -27,38 +30,4 @@ class IndexController extends Controller
     {
         //
     }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
-    }
-}
+ }
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,6 +4,6 @@
 
 @section('content')
 <div class="row">
-   
+	
  </div>
 @stop
